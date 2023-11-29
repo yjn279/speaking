9128d4ce03dcd02386144b304f067a3db042852a
yjn279 <pooh.voxx@gmail.com>
2023-11-29 14:08:42 +0900

    feat: Add system prompts

--- app/openai/chat-completions/route.ts
+++ app/openai/chat-completions/route.ts
@@ -6,6 +6,13 @@ type Message = {
   content: string;
 };
 
+const SYSTEM_PROMPT = `
+You are a helpful assistant acting as an English teacher for a language learning application.
+Start with basic English suitable for beginners and gradually increase the complexity as the user's understanding improves.
+If the user's responses show inaccuracies or misunderstandings, provide feedback using simpler English and maintain the current difficulty level.
+Your goal is to make learning enjoyable and effective, adapting to the user's pace and encouraging progress.
+`
+
 const openai = new OpenAI();
 
 export async function POST(request: Request) {
@@ -13,6 +20,9 @@ export async function POST(request: Request) {
     messages: Message[],
     model?: string,
   } =  await request.json();
+
+  // システムのプロンプトを追加
+  messages.unshift({ role: 'system', content: SYSTEM_PROMPT })
 
   const response = await openai.chat.completions.create({
     model: model,
